d8bdad0b20d1b156729ab71308627021c37cf701
kickflipmycat <132964682+kickflipmycat@users.noreply.github.com>
2025-01-02 04:04:02 +1030

    update Study/Python/mac_changer.py

--- Study/Python/mac_changer.py
+++ Study/Python/mac_changer.py
@@ -20,5 +20,5 @@ def change_mac(interface, new_mac):
   subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
   subprocess.call(["ifconfig", interface, "up"])
   
- = get_arguments()
+ options = get_arguments()
  change_mac(options.interface, options.new_mac)
