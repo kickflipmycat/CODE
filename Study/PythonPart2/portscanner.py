a6ba71f68034cfd456950c1c4aa6a57ab021676f
kickflipmycat <132964682+kickflipmycat@users.noreply.github.com>
2025-01-04 02:57:06 +1030

    update Study/PythonPart2/portscanner.py

--- Study/PythonPart2/portscanner.py
+++ Study/PythonPart2/portscanner.py
@@ -1,4 +1,11 @@
 import socket
 from IPy import IP
 
-sock = socket.socket()
+ipaddress = input("[+] Enter target IP:")
+port = 80
+
+try:
+  sock = socket.socket()
+  sock.connect((ipaddress, port))
+except:
+  print("[+] Port is closed")
